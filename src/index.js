69455e776efc7561013feaca07fa05271e65f8bb
hxdegawa <hxdegawa@gmail.com>
2019-06-10 19:13:17 +0900

    Added anti-porn function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Discord from "discord.js";
 import dotenv from "dotenv";
+import Clarifai from "clarifai";
 
 const controls = [
   { name: "get", description: "Will return this message!" },
@@ -18,10 +19,10 @@ client.on("guildMemberAdd", member => {
   const channel = member.guild.channels.find(
     channel => channel.name === "welcome"
   );
+
   if (channel === undefined) return;
 
-  const isBot = member.user.bot;
-  if (isBot) return;
+  if (member.user.bot) return;
 
   const noobRole = member.guild.roles.find(role => role.name === "Stranger");
 
@@ -44,9 +45,15 @@ client.on("message", message => {
     return;
   }
 
-  if (firstQuote === ".help") displayHelp(message);
+  if (message.author.bot) return;
+
+  // COMMAND DETECTION
+
+  if (firstQuote === ".help")  displayHelp(message);
 
   if (firstQuote === ".timer") setTimer(message);
+
+  if (message.attachments.find(attachments => inspectImage(attachments.url, message)));
 });
 
 function validateMessage(message) {
@@ -89,6 +96,26 @@ async function displayHelp(message) {
   await message.channel.send(embedMessage);
 }
 
-function setTimer() {}
+const setTimer = () => {
+
+}
+
+const inspectImage = async (url, message) => {
+  const clarifai = new Clarifai.App({
+    apiKey: process.env.CLARIFAI_TOKEN
+   });
+
+  clarifai.models.predict("e9576d86d2004ed1a38ba0cf39ecb4b1", url)
+  .then(async response => {
+    if (response.outputs[0].data.concepts[0].name === "nsfw") {
+      const suspendedImage = new Discord.Attachment(url, "SPOILER_nsfw.png");
+      await message.channel.send("よくない画像っぽい気配を感じたので隠します！", suspendedImage);
+
+      await message.delete();
+    }
+    console.log(response.outputs[0].data);
+  })
+  .catch(console.error);
+}
 
 client.login(process.env.BOT_TOKEN);
