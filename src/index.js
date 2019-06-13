7704382ca5037a875ad10a5e3a63de5365a590f6
hxdegawa <hxdegawa@gmail.com>
2019-06-14 00:36:13 +0900

    Implemented radio function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import Discord from "discord.js";
 import dotenv from "dotenv";
 import Clarifai from "clarifai";
+import request from 'request-promise';
+import m3u8stream from 'm3u8stream';
+import cron from 'node-cron';
+import { format, getSeconds, subMinutes } from 'date-fns'
+import { Base64 } from 'js-base64';
 
 const controls = [
-  { name: "get", description: "Will return this message!" },
-  { name: "timer", description: "Sorry, no functions are working right now..." }
+  { name: "help", description: "Will return this message!" },
+  { name: "timer", description: "Sorry, no functions are working right now..." },
+  { name: "play", description: "Plays radio! (type `.play help` for more info)" },
+  { name: "stop", description: "Stops playing radio." }
 ];
 
 dotenv.config();
 
 const client = new Discord.Client();
+let streamCron = false;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -53,6 +61,10 @@ client.on("message", message => {
 
   if (firstQuote === ".timer") setTimer(message);
 
+  if (firstQuote === ".play")  playRadio(message);
+
+  if (firstQuote === ".stop")  stopRadio(message);
+
   if (message.attachments.find(attachments => inspectImage(attachments.url, message)));
 });
 
@@ -85,7 +97,7 @@ async function displayHelp(message) {
   await embedMessage
     .setAuthor(
       "Whipple",
-      "https://www.dropbox.com/s/oguwvbc5kqhaly8/Whipple_slow.gif?dl=1"
+      process.env.BOT_ICON
     )
     .setColor(0xfa8231);
 
@@ -110,12 +122,153 @@ const inspectImage = async (url, message) => {
     if (response.outputs[0].data.concepts[0].name === "nsfw") {
       const suspendedImage = new Discord.Attachment(url, "SPOILER_nsfw.png");
       await message.channel.send("よくない画像っぽい気配を感じたので隠します！", suspendedImage);
-
       await message.delete();
     }
     console.log(response.outputs[0].data);
   })
   .catch(console.error);
+}
+
+const getToken = (connection, station) => {
+
+  const v1Options = {
+    uri: 'https://radiko.jp/v2/api/auth1',
+    transform: (body, response, resolveWithFullResponse) => {
+      return response
+    },
+    headers: {
+      'Content-Type'        : "application/json",
+      'X-Radiko-App'        : "pc_html5",
+      'X-Radiko-App-Version': "0.0.1",
+      'X-Radiko-User'       : "Whipple",
+      'X-Radiko-Device'     : "pc"
+    },
+    json: true
+  };
+
+  request(v1Options)
+  .then(response => {
+    getKey(
+      response.headers["x-radiko-authtoken"],
+      response.headers["x-radiko-keylength"],
+      response.headers["x-radiko-keyoffset"],
+      connection,
+      station
+    );
+  })
+  .catch(err => {
+    console.error(err);
+  })
+}
+
+const getKey = async (token, keyLength, keyOffset, connection, station) => {
+  const v2Options = {
+    uri: 'https://radiko.jp/v2/api/auth2',
+    transform: (body, response, resolveWithFullResponse) => {
+      return response
+    },
+    headers: {
+      'Content-Type'        : "application/json",
+      'X-Radiko-AuthToken'  : token,
+      'X-Radiko-PartialKey' : calcToken(keyLength, keyOffset),
+      'X-Radiko-User'       : "Whipple",
+      'X-Radiko-Device'     : "pc"
+    },
+    json: true
+  };
+
+  request(v2Options)
+  .then(() => {
+    playAudio(token, connection, station);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+}
+
+const calcToken = (keyLength, keyOffset) => {
+  const baseKey = process.env.RADIKO_KEY;
+  const partialKey = baseKey.slice(keyOffset, parseInt(keyLength, 10) + parseInt(keyOffset, 10));
+  return Base64.encode(partialKey);
+}
+
+const playAudio = (token, connection, station) => {
+
+  const m3u8Options = {
+    uri: `https://radiko.jp/v2/api/ts/playlist.m3u8?station_id=${station}&l=15&ft=${format(subMinutes(new Date() , 2), 'YYYYMMDDHHmm00')}&to=${format(subMinutes(new Date(), 1), 'YYYYMMDDHHmm00')}`,
+    transform: body => {
+      return body
+    },
+    headers: {
+      'X-Radiko-AuthToken': token
+    },
+    json: true
+  };
+
+  request(m3u8Options)
+  .then(data => {
+    connection.playStream(m3u8stream(data.split("\n")[3]), {volume: 0.1});
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+const playRadio = async message => {
+  const availableFreq = {
+    'TBS'            : {name: "TBSラジオ"},
+    'QRR'            : {name: "文化放送"},
+    'LFR'            : {name: "ニッポン放送"},
+    'INT'            : {name: "InterFM897"},
+    'FMJ'            : {name: "J-WAVE"},
+    'JORF'           : {name: "ラジオ日本"},
+    'BAYFM78'        : {name: "Bayfm78"},
+    'NACK5'          : {name: "NACK5"},
+    'YFM'            : {name: "FMヨコハマ"},
+    'RN1'            : {name: "ラジオNIKKEI第1"},
+    'RN2'            : {name: "ラジオNIKKEI第2"},
+    'HOUSOU-DAIGAKU' : {name: "放送大学"}
+  }
+
+  const fullMessage = message.content.trim().split(/ |　/);
+
+  if (fullMessage[1] === 'help' || fullMessage[1] === undefined) {
+    const embedMessage = new Discord.RichEmbed();
+
+    await embedMessage
+    .setAuthor("Available radio stations below...", process.env.BOT_ICON)
+    .setColor(0xfa8231)
+    .addField('To use radio, please send me with following format:\n```.play TBS```', 'This "TBS" is the id of radio station, and can be replaced with those station ids.')
+    .addBlankField();
+
+    await Object.keys(availableFreq).forEach(key => {
+      embedMessage.addField(availableFreq[key].name, '`' + key + '`', true);
+    });
+
+    await message.channel.send(embedMessage);
+    return;
+  }
+
+  if (!message.member.voiceChannel) {
+    message.channel.send("You need to join a voice channel first!");
+    return;
+  }
+
+  if (fullMessage[1] in availableFreq) {
+    streamCron ? streamCron.destroy() : null;
+    message.member.voiceChannel.join().then(connection => {
+      getToken(connection, fullMessage[1]);
+      streamCron = cron.schedule(`${getSeconds(new Date())} * * * * *`, () => {
+        getToken(connection, fullMessage[1]);
+      });
+    })
+  } else {
+    message.channel.send(`Station ID '${fullMessage[1]}' doesn't exists in our list of stations...`);
+  }
+}
+
+const stopRadio = message => {
+  message.member.voiceChannel.leave()
 }
 
 client.login(process.env.BOT_TOKEN);
